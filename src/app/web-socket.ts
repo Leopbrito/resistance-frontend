75d3297d9665dfafc7c5383b15b4eaa4e0a29117
Leonardo Brito <leopbrito@icloud.com>
2026-03-07 22:58:57 -0300

    feat: Configure production WebSocket endpoint, update page title, apply global background color, and adjust viewport settings.

--- src/app/web-socket.ts
+++ src/app/web-socket.ts
@@ -1,5 +1,5 @@
   import { io } from "socket.io-client";
 
-  export const socket = io("ws://localhost:3000", {
+  export const socket = io("ws://api-resistance.leopbrito.com/", {
     transports: ['websocket'] 
   });
